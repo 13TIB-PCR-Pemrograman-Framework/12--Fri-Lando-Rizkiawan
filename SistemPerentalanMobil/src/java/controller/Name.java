572491b35ac7f67fa202c7c1098fd20d1f22ff96
LabGSG <LabGSG@LABGSG-PC5>
2015-10-13 09:29:21 +0700

    pertama

---
+++ SistemPerentalanMobil/src/java/controller/Name.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+/**
+ *
+ * @author LabGSG
+ */
+public class Name {
+    private String value;
+    private String age;
+    
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+    
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+}
